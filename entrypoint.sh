4abbc0b5249859a5a341c76372865d9c16fdc897
Elrondo46 <18145437+Elrondo46@users.noreply.github.com>
2026-08-25 12:12:52 +0200

    DotClear changed their fuck... version TODAY

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,8 +79,8 @@ dotclear)
   if [ -f /var/www/html/drupal/web/"$FILETEST" ] || [ -f /var/www/html/"$FILETEST" ]; then
     echo "DotClear or another CMS already installed, Install nothing"
   else
-   wget "https://download.dotclear.org/latest/dotclear-2.39.2.zip"
-   unzip dotclear-2.39.2.zip -d /var/www/html
+   wget "https://download.dotclear.org/latest/dotclear-2.39.3.zip"
+   unzip dotclear-2.39.3.zip -d /var/www/html
    mv dotclear/* .
    mv dotclear/.* .
    rm -r dotclear
